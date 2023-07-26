c3ac5a1d02f75b0d1e614c88eed55fec436e4ed0
Leah Le <phnganh.le@gmail.com>
2023-07-27 02:39:16 +0700

    add projects

--- src/components/projectList/projectList.js
+++ src/components/projectList/projectList.js
@@ -8,12 +8,12 @@ const ProjectList = () => {
       <div className="pl-texts">
         <h1 className="pl-title">Projects</h1>
         <p className="pl-desc">
-          Where I put my learnings into practice
+          Where I put my learnings into practice. Click on each project to see more details.
         </p>
       </div>
       <div className="pl-list">
         {projects.map((item) => (
-          <Project key={item.id} img={item.img} link={item.link} />
+          <Project key={item.id} img={item.img} link={item.link}/>
         ))}
       </div>
     </div>
--- src/data.js
+++ src/data.js
@@ -1,18 +1,34 @@
 export const projects = [
+    
     {
       id: 1,
-      img: "https://i.ibb.co/RPV0pkY/project1.png",
-      link: "https://github.com/Godsheritage/palateparadise",
+      img: "https://i.ibb.co/PDqXpYP/Screenshot-2023-07-27-at-1-49-43-AM.png",
+      link: "https://github.com/leahphw/python-battleship",
     },
     {
       id: 2,
-      img: "",
-      link: "http://lama.dev",
+      img: "https://i.ibb.co/QrPp8jD/Screenshot-2023-07-27-at-2-31-28-AM.png",
+      link: "https://github.com/leahphw/ai-nft-generator",
     },
     {
       id: 3,
-      img: "",
-      link: "http://lama.dev",
+      img: "https://i.ibb.co/4MF7M8k/screencapture-thegraph-studio-subgraph-pgxliquidity-playground-2023-07-27-02-34-56.png",
+      link: "https://github.com/leahphw/pgx-liquidity-subgraph",
+    },
+    {
+      id: 4,
+      img: "https://i.ibb.co/ckJSM7L/Screenshot-2023-07-27-at-1-47-12-AM.png",
+      link: "https://github.com/Godsheritage/palateparadise",
+    },
+    {
+      id: 5,
+      img: "https://i.ibb.co/QHtV5K9/Screenshot-2023-07-27-at-1-37-27-AM.png",
+      link: "https://github.com/leahphw/bereal-clone",
+    },
+    {
+      id: 6,
+      img: "https://i.ibb.co/92ZVPXx/Screenshot-2023-07-27-at-2-24-43-AM.png",
+      link: "https://github.com/leahphw/pixishare-dapp",
     },
   ];
   
