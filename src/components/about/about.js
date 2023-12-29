b641b7e0260c3f29a7b6369d48d7e442aa19074c
leahphw <leah.phw@gmail.com>
2023-12-29 16:57:08 +0700

    redeploy

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -16,7 +16,7 @@ const About = () => {
       <div className="a-right">
         <h1 className="a-title">About Me</h1>
         <p className="a-desc">
-          As a rising sophomore pursuing a dual major in Computer Science and Finance, I am deeply passionate about the transformative potential of technology and finance. My interests lie at the intersection of three dynamic fields: blockchain technology, data engineering, and fintech solutions.
+          As a college sophomore pursuing a dual major in Computer Science and Finance, I am deeply passionate about the transformative potential of technology and finance. My interests lie at the intersection of three dynamic fields: blockchain technology, data engineering, and fintech solutions.
         </p>
         <p className="a-desc">
         With a solid foundation in programming languages and data analysis, I am eager to explore the vast possibilities of blockchain technology. Its decentralized nature and ability to revolutionize industries like finance and supply chain have captivated me. I continuously hone my technical skills, driven by the desire to contribute to this groundbreaking domain.</p>
@@ -24,7 +24,7 @@ const About = () => {
           <div className="a-award-texts">
             <h4 className="a-award-title">Involvements</h4>
             <p className="a-award-desc">
-              University of Richmond ICPC Team Member, Girls Who Code, Association of Computing Machinery
+              University of Richmond ICPC Team Member, Girls Who Code Vice President, Association of Computing Machinery
             </p>
           </div>
         </div>
